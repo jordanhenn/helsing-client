3a1e27fe48f707977c656953cdb4117c13456be2
Jordan Henn <jordanwalkerhenn@gmail.com>
2021-03-27 22:44:26 -0700

    favicon & title

--- src/config.js
+++ src/config.js
@@ -1,5 +1,3 @@
 export default {
-    API_ENDPOINT: 'http://localhost:8000/api',
+    API_ENDPOINT: process.env.REACT_APP_API_BASE_URL || 'http://localhost:8000/api',
   }
-
-  //process.env.REACT_APP_API_BASE_URL ||
